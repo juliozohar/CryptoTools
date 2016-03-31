7481c88d32c0397d2bf0eeae68099aa3e48eee2a
Julio Cesar Torres <juliozohar@gmail.com>
2016-03-30 22:59:57 -0400

    Initial Commit

---
+++ CryptoTools/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CryptoTools
+//
+//  Created by Julio Cesar Torres dos Santos on 3/30/16.
+//  Copyright (c) 2016 Julio Cesar Torres dos Santos. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
